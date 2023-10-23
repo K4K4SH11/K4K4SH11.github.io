8c5d58bec30e71fcadc0505847e86c58c089fc07
K4K4SH11 <39433002+K4K4SH11@users.noreply.github.com>
2023-10-23 14:12:39 +0100

    Update region.js

--- api/region.js
+++ api/region.js
@@ -1,1 +1,17 @@
-
+const curr=window.location.href;
+const page=curr.split("?")[1]
+if (page==="region") {
+  fetch("https://ipapi.co/json/")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error: ' + error.message);
+    });
+}
